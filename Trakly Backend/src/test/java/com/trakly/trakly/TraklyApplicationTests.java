2bae813dd68c769e382c88291a97764fc7cf8d85
Bryan Vela A <55074395+bdvela@users.noreply.github.com>
2021-06-22 16:05:11 -0500

    Company implementation

---
+++ Trakly Backend/src/test/java/com/trakly/trakly/TraklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trakly.trakly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
